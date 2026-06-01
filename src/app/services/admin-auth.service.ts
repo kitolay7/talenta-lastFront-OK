19f2163701fef7ffc762a3e48c71000b253c5476
kitolay7 <ralitera.toky7@gmail.com>
2026-06-01 07:52:45 +0300

    commit010626

--- src/app/services/admin-auth.service.ts
+++ src/app/services/admin-auth.service.ts
@@ -17,23 +17,4 @@ export class AdminAuthService {
     try { localStorage.removeItem(this.KEY); } catch {}
   }
   
-  /*
-  private isAuthenticated = new BehaviorSubject<boolean>(false);
-
-  login(username: string, password: string): boolean {
-    if (username === 'admin' && password === '@dm!n2O25') {
-      this.isAuthenticated.next(true);
-      return true;
-    }
-    return false;
-  }
-
-  logout() {
-    this.isAuthenticated.next(false);
-  }
-
-  isAdmin(): Observable<boolean> {
-    return this.isAuthenticated.asObservable();
-  }
-    */
 }
